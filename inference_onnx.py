03c57485c54e00f644c4c35f78eb7767239adee3
SheldonFung98 <sheldonvonsr@gmail.com>
2024-12-26 17:39:36 +0800

    fixed some bugs

--- inference_onnx.py
+++ inference_onnx.py
@@ -2,7 +2,6 @@ import torch
 import torch.nn as nn
 import numpy as np
 import time
-from models.procrustes import SoftProcrustesLayer
 
 import cpp_wrappers.cpp_neighbors.radius_neighbors as cpp_neighbors
 import cpp_wrappers.cpp_subsampling.grid_subsampling as cpp_subsampling
@@ -10,6 +9,103 @@ from pointscope import PointScopeClient as PSC
 
 import onnxruntime as ort
 import open3d as o3d
+
+def topk(data, num_topk):
+    sort, idx = data.sort(descending=True)
+    return sort[:num_topk], idx[:num_topk]
+
+class SoftProcrustesLayer(nn.Module):
+    def __init__(self, config):
+        super(SoftProcrustesLayer, self).__init__()
+
+        self.sample_rate = config.sample_rate
+        self.max_condition_num = config.max_condition_num
+
+    @staticmethod
+    def batch_weighted_procrustes( X, Y, w, eps=0.0001):
+        '''
+        @param X: source frame [B, N,3]
+        @param Y: target frame [B, N,3]
+        @param w: weights [B, N,1]
+        @param eps:
+        @return:
+        '''
+        # https://ieeexplore.ieee.org/document/88573
+
+        bsize = X.shape[0]
+        device = X.device
+        W1 = torch.abs(w).sum(dim=1, keepdim=True)
+        w_norm = w / (W1 + eps)
+        mean_X = (w_norm * X).sum(dim=1, keepdim=True)
+        mean_Y = (w_norm * Y).sum(dim=1, keepdim=True)
+        Sxy = torch.matmul( (Y - mean_Y).transpose(1,2), w_norm * (X - mean_X) )
+        Sxy = Sxy.cpu().double()
+        U, D, V = Sxy.svd() # small SVD runs faster on cpu
+        condition = D.max(dim=1)[0] / D.min(dim=1)[0]
+        S = torch.eye(3)[None].repeat(bsize,1,1).double()
+        UV_det = U.det() * V.det()
+        S[:, 2:3, 2:3] = UV_det.view(-1, 1,1)
+        svT = torch.matmul( S, V.transpose(1,2) )
+        R = torch.matmul( U, svT).float().to(device)
+        t = mean_Y.transpose(1,2) - torch.matmul( R, mean_X.transpose(1,2) )
+        return R, t, condition
+
+
+
+    def forward(self,  conf_matrix,  src_pcd, tgt_pcd,  src_mask=None, tgt_mask=None):
+        '''
+        @param conf_matrix:
+        @param src_pcd:
+        @param tgt_pcd:
+        @param src_mask:
+        @param tgt_mask:
+        @return:
+        '''
+
+        bsize, N, M = conf_matrix.shape
+
+        # subsample correspondence
+        if src_mask is None:
+            src_len = torch.tensor([N])
+        else:
+            src_len = src_mask.sum(dim=1)
+        if tgt_mask is None:
+            tgt_len = torch.tensor([M])
+        else:
+            tgt_len = tgt_mask.sum(dim=1)
+
+        entry_max, _ = torch.stack([src_len,tgt_len], dim=0).max(dim=0)
+        entry_max = (entry_max * self.sample_rate).int()
+        sample_n_points = entry_max.float().mean().int() #entry_max.max()
+        conf, idx = conf_matrix.view(bsize, -1).sort(descending=True,dim=1)
+        w = conf [:, :sample_n_points]
+        idx= idx[:, :sample_n_points]
+        idx_src = idx//M #torch.div(idx, M, rounding_mode='trunc')
+        idx_tgt = idx%M
+        b_index = torch.arange(bsize).view(-1, 1).repeat((1, sample_n_points)).view(-1)
+        src_pcd_sampled = src_pcd[b_index, idx_src.view(-1)].view(bsize, sample_n_points, -1)
+        tgt_pcd_sampled = tgt_pcd[b_index, idx_tgt.view(-1)].view(bsize, sample_n_points, -1)
+        w_mask = torch.arange(sample_n_points).view(1,-1).repeat(bsize,1).to(w)
+        w_mask = w_mask < entry_max[:,None]
+        w[~w_mask] = 0.
+
+        # solve
+        try :
+            R, t, condition = self.batch_weighted_procrustes(src_pcd_sampled, tgt_pcd_sampled, w[...,None])
+        except: # fail to get valid solution, this usually happens at the early stage of training
+            R = torch.eye(3)[None].repeat(bsize,1,1).type_as(conf_matrix)
+            t = torch.zeros(3, 1)[None].repeat(bsize,1,1).type_as(conf_matrix)
+            condition = torch.zeros(bsize).type_as(conf_matrix)
+
+        #filter unreliable solution with condition nnumber
+        solution_mask = condition < self.max_condition_num
+        R_forwd = R.clone()
+        t_forwd = t.clone()
+        R_forwd[~solution_mask] = torch.eye(3).type_as(R)
+        t_forwd[~solution_mask] = torch.zeros(3, 1).type_as(R)
+
+        return R, t, R_forwd, t_forwd, condition, solution_mask
+
 
 def batch_neighbors_kpconv(queries, supports, q_batches, s_batches, radius, max_neighbors):
     """
@@ -235,8 +331,8 @@ class Pipeline(nn.Module):
             'onnx_export/transformer.onnx', 
         )
         self.max_points = 15000
-        # self.rt_method = "RANSAC"
-        self.rt_method = "SoftProcrustes"
+        self.rt_method = "RANSAC"
+        # self.rt_method = "SoftProcrustes"
         if self.rt_method == "SoftProcrustes":
             self.coarse_matching = ort.InferenceSession('onnx_export/matching.onnx')
             self.soft_procrustes = SoftProcrustesLayer(config['coarse_transformer']['procrustes'])
@@ -289,11 +385,11 @@ class Pipeline(nn.Module):
 
         point_num_diff = s_pcd.shape[0] - t_pcd.shape[0]
         if point_num_diff > 0:
-            t_pcd = np.concatenate([t_pcd, np.zeros_like(t_pcd[:point_num_diff])])
-            tgt_feats = np.concatenate([tgt_feats, np.zeros_like(tgt_feats[:point_num_diff])])
+            t_pcd = np.concatenate([t_pcd, np.zeros((point_num_diff, t_pcd.shape[-1]))]).astype(np.float32)
+            tgt_feats = np.concatenate([tgt_feats, np.zeros((point_num_diff, tgt_feats.shape[-1]))]).astype(np.float32)
         elif point_num_diff < 0:
-            s_pcd = np.concatenate([s_pcd, np.zeros_like(s_pcd[:point_num_diff])])
-            src_feats = np.concatenate([src_feats, np.zeros_like(src_feats[:point_num_diff])])
+            s_pcd = np.concatenate([s_pcd, np.zeros((-point_num_diff, s_pcd.shape[-1]))]).astype(np.float32)
+            src_feats = np.concatenate([src_feats, np.zeros((-point_num_diff, src_feats.shape[-1]))]).astype(np.float32)
 
         src_feats_cond, tgt_feats_cond, src_pe, tgt_pe = self.coarse_transformer.run(None, {
             "src_feats": src_feats[None, ...],
@@ -315,8 +411,8 @@ class Pipeline(nn.Module):
         if self.rt_method == "RANSAC":
             src_pcd = to_o3d_pcd(s_pcd)
             tgt_pcd = to_o3d_pcd(t_pcd)
-            src_feats = to_o3d_feats(src_feats_cond[0, : src_pcd.shape[0]])
-            tgt_feats = to_o3d_feats(tgt_feats_cond[0, : tgt_pcd.shape[0]])
+            src_feats = to_o3d_feats(src_feats_cond[0, : s_pcd.shape[0]])
+            tgt_feats = to_o3d_feats(tgt_feats_cond[0, : t_pcd.shape[0]])
             distance_threshold = 0.05
             ransac_n = 3
             result_ransac = o3d.pipelines.registration.registration_ransac_based_on_feature_matching(
@@ -362,6 +458,7 @@ class Pipeline(nn.Module):
         PSC().add_pcd(src_pred[:, :-1]).add_pcd(tgt).show()
 
         src_ = torch.from_numpy(src).float()
+        # src_ = src.clone()
         src_h = torch.cat([src_, torch.ones_like(src_[:, :1])], dim=1)
         src_gt_trans = src_h @ src_gt.T @ tgt_gt.T.inverse()
         PSC().add_pcd(src_gt_trans[:, :-1]).add_pcd(tgt).show()
@@ -391,13 +488,15 @@ if __name__ == '__main__':
     
     pipeline = Pipeline(config)
 
-    root_dir = "/home/sheldon/sharefd/data/indoor/test/7-scenes-redkitchen"
-    src_file = os.path.join(root_dir, "cloud_bin_0.pth")
-    tgt_file = os.path.join(root_dir, "cloud_bin_1.pth")
+    # scene = "7-scenes-redkitchen"
+    scene = "sun3d-hotel_uc-scan3"
+    root_dir = "/home/sheldon/sharefd/data/indoor/test"
+    src_file = os.path.join(root_dir, scene, "cloud_bin_0.pth")
+    tgt_file = os.path.join(root_dir, scene, "cloud_bin_2.pth")
 
-    root_dir = "/home/sheldon/sharefd/data/data/"
-    src_file = os.path.join(root_dir, "oriMesh1_1.ply")
-    tgt_file = os.path.join(root_dir, "oriMesh3_1.ply")
+    # root_dir = "/home/sheldon/sharefd/data/data/"
+    # src_file = os.path.join(root_dir, "oriMesh1_1.ply")
+    # tgt_file = os.path.join(root_dir, "oriMesh3_1.ply")
 
     src = load_pcd(src_file)
     tgt = load_pcd(tgt_file)
